103a5ea8a696656190e996665a9045099b913027
罗鸿胤 <lhy@30-9-227.wireless.csail.mit.edu>
2016-09-30 14:16:32 -0400

    commit on MAC

---
+++ mix_data.py
@@ -0,0 +1,20 @@
+#lhy
+#2016.9
+
+import random
+
+if __name__ == "__main__":
+    src_train = open(r'data/src-train.txt').readlines()
+    targ_train = open(r'data/targ-train.txt').readlines()
+    src_handle = open("data/scr-train-mixed.txt", 'w')
+    targ_handle = open("data/targ-train-mixed.txt", 'w')
+    src = map((lambda x : x.strip('\n')), src_train)
+    targ = map((lambda x : x.strip('\n')), targ_train)
+    corpus1 = map((lambda x, y : [x, y]), src, targ)
+    corpus2 = map((lambda x, y: [x, y]), targ, src)
+    corpus1.extend(corpus2)
+    random.shuffle(corpus1)
+    src = map((lambda x : x[0]), corpus1)
+    targ = map((lambda x : x[1]), corpus1)
+    src_handle.write('\n'.join(src))
+    targ_handle.write('\n'.join(targ))
