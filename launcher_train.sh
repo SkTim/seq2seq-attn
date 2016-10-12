53402e14387ef5c6a9fb8c5e291a43d333a20cba
SkTim <lhythu@gmail.com>
2016-10-12 16:00:31 -0400

    commit on SLS-TITAN-5

--- launcher_train.sh
+++ launcher_train.sh
@@ -1,5 +1,7 @@
 #lhy
 #2016.9
 
-th train_onlinegrad.lua -data_file data/demo-train.hdf5 -val_data_file data/demo-val.hdf5 -savefile models/demo-model
+#th train.lua -data_file data/demo-train.hdf5 -val_data_file data/demo-val.hdf5 -savefile models/demo-model
+th train_onlinegrad.lua -epochs 6 -train_from raw_code/models/demo-model_epoch7.00_30.97.t7\
+	-data_file raw_code/data/demo-train.hdf5 -val_data_file raw_code/data/demo-val.hdf5 -savefile models/BDT-1-FROM7
 #th train.lua -data_file data/double-st-train.hdf5 -val_data_file data/double-st-val.hdf5 -savefile models/double-st-model
